23823539148c386b69a682f5ee47bf0163546fbb
Demetrio <dlima.nyit@gmail.com>
2023-08-15 15:38:42 -0700

    Double checking work and organizing code

--- index.js
+++ index.js
@@ -1,82 +1,116 @@
-document.addEventListener('DOMContentLoaded', () => fetchGames(), fetchCart())
+const cartButton = document.getElementById("cart-button");  
+const form = document.querySelector("form");
+const cartQuantity = document.querySelector("#cart-quantity");
+const toggleCart = document.getElementById("toggleCart");
+const cartTotalValue = document.getElementById("cart-total-value");
+const cartTotalItems = []
+
+document.addEventListener('DOMContentLoaded', () => {
+    fetchGames() 
+    fetchCart()
+})
+
 
 function fetchGames(){
     fetch("http://localhost:3000/games")
     .then((resp) => resp.json())
-    .then((games) => renderGames(games));
+    .then((games) => games.forEach(game => renderGame(game)));
 }
 
-function renderGames(games){
-    games.forEach((game) => {
-        const container = document.querySelector("#game-container");
-        const gameDiv = document.createElement("div");
-        container.append(gameDiv);
-        gameDiv.className = "game";
-        gameDiv.id = game.id;   
+function renderGame(game){
+    const container = document.querySelector("#game-container");
+    const gameDiv = document.createElement("div");
+    container.append(gameDiv);
+    gameDiv.className = "game";
+    gameDiv.id = game.id;   
 
-        const gameName = document.createElement("h2");
-        gameName.textContent = game.name;
+    const gameName = document.createElement("h2");
+    gameName.textContent = game.name;
 
-        const img = document.createElement("img");
-        img.src = game.image;
-        img.alt = game.name;
+    const img = document.createElement("img");
+    img.src = game.image;
+    img.alt = game.name;
         
-        const price = document.createElement("p");
-        price.id = "price";
-        price.textContent = `Price: $${game.price}`;
+    const price = document.createElement("p");
+    price.id = "price";
+    price.textContent = `Price: $${game.price}`;
 
-        const release = document.createElement("p");
-        release.textContent = `Release: ${game.release}`;
+    const release = document.createElement("p");
+    release.textContent = `Release: ${game.release}`;
 
-        const genre = document.createElement("p");
-        genre.textContent = `Genre: ${game.genre}`;
+    const genre = document.createElement("p");
+    genre.textContent = `Genre: ${game.genre}`;
 
-        const mode = document.createElement("p");
-        mode.textContent = `Mode: ${game.mode}`
+    const mode = document.createElement("p");
+    mode.textContent = `Mode: ${game.mode}`
 
-        const btn = document.createElement("button");
-        btn.className = "buybtn";
-        btn.textContent = "Add to Cart";
-        btn.addEventListener("click", () => {
-            fetch("http://localhost:3000/cartItems", {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
-                    "Accept": "application/json"
-                },
-                body: JSON.stringify({
-                    cartId: game.id,
-                    price: game.price,
-                    name: game.name,
-                    image: game.image,
-                    quantity: 1
-                })
-            })
-            .then((resp) => resp.json())
-            .then((newCartItems) => {
-                cartItemsArray.push(newCartItems);
-                renderAllCartItems(cartItemsArray);
-            });
-        }); 
-        gameDiv.append(gameName, img, price, release, genre, mode, btn);
-    });
+    const btn = document.createElement("button");
+    btn.className = "buybtn";
+    btn.textContent = "Add to Cart";
+    btn.addEventListener("click", () => addToCart(game)); 
+    gameDiv.append(gameName, img, price, release, genre, mode, btn);
+
 }
 
-const cartQuantity = document.querySelector("#cart-quantity");
-const toggleCart = document.getElementById("toggleCart");
-const cartTotalValue = document.getElementById('cart-total-value');
+function addToCart(game){
+    const checkCartItem = cartTotalItems.find(cartItem => cartItem.gameId === game.id)
+    if(checkCartItem){
+        const newQuantity = checkCartItem.quantity + 1
+        fetch(`http://localhost:3000/cartItems/${checkCartItem.id}`,{
+            method: "PATCH",
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json"
+            },
+            body: JSON.stringify({
+                price: game.price * newQuantity,
+                quantity: newQuantity
+            })
+        })
+        .then(resp => resp.json())
+        .then(updatedCartItem => {
+            const cartItemCard = document.getElementById(updatedCartItem.id);
+            console.log(cartItemCard)
+        })
+    } else { 
+        fetch("http://localhost:3000/cartItems", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json"
+            },
+            body: JSON.stringify({
+                gameId: game.id,
+                price: game.price,
+                name: game.name,
+                image: game.image,
+                quantity: 1
+            })
+        })
+        .then((resp) => resp.json())
+        .then((newCartItem) => {
+            updateCart(newCartItem)
+        });
+    }
+
+}
+
+function updateCart(cartItem){
+    console.log(cartItem)
+}
+
 
 function fetchCart () {
     fetch("http://localhost:3000/cartItems")
     .then(resp => resp.json())
-    .then(cartItems => renderAllCartItems(cartItems));
+    .then(cartItems => 
+        cartItems.forEach(cartItem => renderCartItem(cartItem)))
 }
 
-function renderAllCartItems(cartItems) {
-  cartItems.forEach((cartItem) => {
+function renderCartItem(cartItem) {
     const cItem = document.createElement('cart-item');
     toggleCart.append(cItem);
-    cItem.id = cartItem.id;
+    cItem.id = cartItem.gameId;
     cItem.className = "cart-item";
 
     const name = document.createElement("h3");
@@ -99,15 +133,18 @@ function renderAllCartItems(cartItems) {
         })
         .then(resp => resp.json())
         .then(cartItems => {
-            renderAllCartItems(cartItems);
+            cItem.remove();
+            cartQuantity.textContent = cartItems.length;
+            cartTotalValue.textContent = cartItems.reduce((total, cartItem) => total + cartItem.price, 0);
         })
     })
     cItem.append(name, img, price, quantity, removeBtn);
-  });
 }
 
-function searchGame() {
-    const form = document.querySelector("form");
+function updateCartElement(cartItem){
+    const cartItemCard = document.getElementById(cartItem.id);
+}
+
     form.addEventListener("submit", (event) => {
         event.preventDefault();
         const input = document.querySelector("input").value.toLowerCase();
@@ -126,15 +163,12 @@ function searchGame() {
             }
         })
     });
-};
-searchGame();
 
-function cartToggler() {
-    const cartButton = document.getElementById("cart-button");
+
     cartButton.addEventListener("click", () => {
         toggleCart.classList.toggle("hidden");
     })
-}
-cartToggler()
+
+
 
 
